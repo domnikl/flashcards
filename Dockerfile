c5f08b764915d33559bb9ae0e1796ce46d6d70db
Dominik Liebler <dominik.liebler@yazio.com>
2024-05-16 14:27:21 +0200

    load Supabase key in client

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,18 @@
-FROM node:21
+FROM node:21 AS build
 WORKDIR /app
 
-COPY . ./
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm ci --silent
 
-# building the app
-RUN npm i
+COPY . ./
 RUN npm run build
 
 # Running the app
 CMD [ "npm", "start" ]
+
+# production environment
+FROM nginx:stable-alpine
+COPY --from=build /app/build /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/supabase.ts
+++ src/supabase.ts
@@ -1,74 +1,77 @@
-import {createClient, User} from '@supabase/supabase-js'
-import {Cardset} from "./model/Cardset";
-import {Card} from "./model/Card";
-import {Quiz} from "./model/Quiz";
+import { createClient, User } from "@supabase/supabase-js";
+import { Cardset } from "./model/Cardset";
+import { Card } from "./model/Card";
+import { Quiz } from "./model/Quiz";
 
-const supabaseUrl = 'https://fsexihplnswtlgywhxkp.supabase.co'
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY!!
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://fsexihplnswtlgywhxkp.supabase.co";
+// @ts-ignore
+const supabaseKey = window.__env__.REACT_APP_SUPABASE_KEY!!;
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 // TODO: better error handling for all queries
 
-export async function findAllCardsetsByUser(user: User | null): Promise<Array<Cardset>> {
-    return supabase
-        .from("cardsets")
-        .select()
-        .eq("user_id", user?.id)
-        .eq("is_deleted", false)
-        .then(({data}) => {
-            return data!! as Cardset[]
-        });
+export async function findAllCardsetsByUser(
+  user: User | null
+): Promise<Array<Cardset>> {
+  return supabase
+    .from("cardsets")
+    .select()
+    .eq("user_id", user?.id)
+    .eq("is_deleted", false)
+    .then(({ data }) => {
+      return data!! as Cardset[];
+    });
 }
 
 export async function findCardsetById(id: string): Promise<Cardset | null> {
-    return supabase
-        .from("cardsets")
-        .select(`id, name, is_deleted, image_url, user_id, cards (*, quiz (*))`)
-        .eq("id", id)
-        .eq("is_deleted", false)
-        .eq("cards.is_deleted", false)
-        .then(({data}) => {
-            return (data?.at(0) ?? null) as Cardset | null;
-        });
+  return supabase
+    .from("cardsets")
+    .select(`id, name, is_deleted, image_url, user_id, cards (*, quiz (*))`)
+    .eq("id", id)
+    .eq("is_deleted", false)
+    .eq("cards.is_deleted", false)
+    .then(({ data }) => {
+      return (data?.at(0) ?? null) as Cardset | null;
+    });
 }
 
 export async function findCardById(id: string): Promise<Card | null> {
-    return supabase
-        .from("cards")
-        .select()
-        .eq("id", id)
-        .eq("is_deleted", false)
-        .then(({data}) => {
-            return (data?.at(0) ?? null) as Card | null;
-        });
+  return supabase
+    .from("cards")
+    .select()
+    .eq("id", id)
+    .eq("is_deleted", false)
+    .then(({ data }) => {
+      return (data?.at(0) ?? null) as Card | null;
+    });
 }
 
 export async function saveCardset(cardset: Cardset, user_id: string) {
-    return supabase
-        .from("cardsets")
-        .upsert([{...cardset, user_id: user_id}])
-        .select()
-        .then(({data}) => {
-            return data!! as Cardset[] | null;
-        });
+  return supabase
+    .from("cardsets")
+    .upsert([{ ...cardset, user_id: user_id }])
+    .select()
+    .then(({ data }) => {
+      return data!! as Cardset[] | null;
+    });
 }
 
 export async function saveCard(card: Card) {
-    return supabase
-        .from("cards")
-        .upsert([{...card}])
-        .select()
-        .then(({data}) => {
-            return data!! as Card[] | null;
-        });
+  return supabase
+    .from("cards")
+    .upsert([{ ...card }])
+    .select()
+    .then(({ data }) => {
+      return data!! as Card[] | null;
+    });
 }
 
 export async function saveQuizzes(quizzes: Set<Quiz>) {
-    return supabase
-        .from("quiz")
-        .upsert([...quizzes])
-        .select()
-        .then(({data}) => {
-            return data!! as Card[] | null;
-        });
+  return supabase
+    .from("quiz")
+    .upsert([...quizzes])
+    .select()
+    .then(({ data }) => {
+      return data!! as Card[] | null;
+    });
 }
